66252bb2fc8566f331bce9d4820799eb3655ade9
WXLWEB <894147172@qq.com>
2015-07-12 14:02:39 +0800

    change arrow left and right

--- app/scripts/carousel.js
+++ app/scripts/carousel.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
     navigation : true, // Show next and prev buttons
     slideSpeed : 300,
     paginationSpeed : 400,
-    singleItem:true
+    singleItem:true,
+    navigationText : ["<i class='iconfont icon-left hidden-xs'>&#xe60d</i>", "<i class='iconfont icon-right hidden-xs'>&#xe60f</i>"]
 
     // "singleItem:true" is a shortcut for:
     // items : 1,
