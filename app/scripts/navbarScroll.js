f76af6042a5afe5430d2ab80b31a62da683e1b00
WXLWEB <894147172@qq.com>
2015-07-19 19:33:56 +0800

    fix logo location

--- app/scripts/navbarScroll.js
+++ app/scripts/navbarScroll.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     t = document.documentElement.scrollTop || document.body.scrollTop;
     if (t > 0) {
       $('.navbar').removeClass('navbar-default').addClass('navbar-default-blue');
-      $('.navbar-brand').css('background', 'url("../images/public/logo2.png") top left no-repeat');
+      $('.navbar-brand').css('background', 'url("images/public/logo2.png") top left no-repeat');
       $('.marker').css('background-color', '#025499');
     } else {
       $('.navbar').removeClass('navbar-default-blue').addClass('navbar-default');
-      $('.navbar-brand').css('background', 'url("../images/public/logo1.png") top left no-repeat');
+      $('.navbar-brand').css('background', 'url("images/public/logo1.png") top left no-repeat');
       $('.marker').css('background-color', '#fff');
     }
   });
