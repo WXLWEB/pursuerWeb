cc5d2ebd6ed026fe4c11fc6bb178712df71643e6
WXLWEB <894147172@qq.com>
2015-07-19 23:47:42 +0800

    navmarker only >=768 effect

--- app/scripts/navMarker.js
+++ app/scripts/navMarker.js
@@ -1,32 +1,35 @@
 $(function() {
   var ws = [];
-  $('.navbar-nav li').each(function(index){
-    ws[index] = $(this).outerWidth();
-  });
-  activeBorder = function(){
-    var activeIndex = $('.navbar-nav li').index($('.navbar .active'));
-    var left=0;
-    for(var i =0; i<activeIndex;i++){
-      left = left+ ws[i];
-    }
-    $('.marker').css({
-      left:left+20,
-      width:ws[activeIndex]-40
+  if($(window).width()>768){
+    $('.navbar-nav li').each(function(index){
+      ws[index] = $(this).outerWidth();
     });
-  };
-  activeBorder();
-  $('.navbar-nav li a').on({
-    click:function(){
-      index  = $(this).parent().index();
+    activeBorder = function(){
+      var activeIndex = $('.navbar-nav li').index($('.navbar .active'));
       var left=0;
-      for(var i =0; i<index;i++){
+      for(var i =0; i<activeIndex;i++){
         left = left+ ws[i];
       }
       $('.marker').css({
         left:left+20,
-        width:ws[index]-40
+        width:ws[activeIndex]-40
       });
-    }
-    }
-  );
+    };
+    activeBorder();
+    $('.navbar-nav li a').on({
+        click:function(){
+          index  = $(this).parent().index();
+          var left=0;
+          for(var i =0; i<index;i++){
+            left = left+ ws[i];
+          }
+          $('.marker').css({
+            left:left+20,
+            width:ws[index]-40
+          });
+        }
+      }
+    );
+  }
+
 });
